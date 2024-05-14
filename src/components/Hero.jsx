595c691c5de8d640ae03c96337666c7cf61aec8b
Syed Humayun Rasheed <104017282+humayunrasheed@users.noreply.github.com>
2024-05-15 01:18:49 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,10 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
+      <div className="hidden md:block">
+        <ComputersCanvas />
+      </div>
+      
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
